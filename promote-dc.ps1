b34cd7204a6220828e86c5cb8689acd63ce78418
Serena Lin <serena.lin0503@outlook.com>
2026-05-06 12:52:45 -0400

    Delete promote-dc.ps1

--- promote-dc.ps1
+++
@@ -1,3 +0,0 @@
-$DSRMPassword = Read-Host -AsSecureString -Prompt "Enter DSRM password"
-
-Install-ADDSForest -DomainName "contoso.local" -DomainNetbiosName "CONTOSO" -ForestMode "WinThreshold" -DomainMode "WinThreshold" -InstallDns:$true -DatabasePath "C:\Windows\NTDS" -LogPath "C:\Windows\NTDS" -SysvolPath "C:\Windows\SYSVOL" -SafeModeAdministratorPassword $DSRMPassword -Force:$true
